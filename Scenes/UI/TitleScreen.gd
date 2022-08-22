d6e40d727b6e4f8bc340138376588c9b486bee62
Alec <84426874+BoimanMan@users.noreply.github.com>
2022-08-21 22:25:55 -0500

    Title Screen Implemented
    Base for title screen implemented, still needs a picture on the right.

---
+++ Scenes/UI/TitleScreen.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var game_start_text = $CanvasLayer/TextureRect/MarginContainer/VBoxContainer/HBoxContainer/Label
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+func _input(event):
+	if event is InputEventKey or event is InputEventMouseButton:
+		get_tree().change_scene("res://Scenes/Levels/Main.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_FlashTimer_timeout():
+	if game_start_text.is_visible():
+		game_start_text.hide()
+	else:
+		game_start_text.show()
