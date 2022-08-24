68027877a67dc789d3b83b4bff41e48a22075f49
Alec <84426874+BoimanMan@users.noreply.github.com>
2022-08-23 20:15:27 -0500

    Title Screen BGM implemented
    Title Screen has placeholder BGM, can skip the intro by pressing any button and can go to the main menu by pressing any button after the intro is done or after skipping said intro.

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -4,16 +4,26 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var flash = $FlashTimer
+onready var bgm = $BGM
 onready var game_start_text = $CanvasLayer/TextureRect/MarginContainer/VBoxContainer/HBoxContainer/Label
-
+onready var intro_finished = false
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 	#pass # Replace with function body.
 func _input(event):
 	if event is InputEventKey or event is InputEventMouseButton:
-		get_tree().change_scene("res://Scenes/UI/MainMenu.tscn")
+		if intro_finished:
+			$BGM.stop()
+			get_tree().change_scene("res://Scenes/UI/MainMenu.tscn")
+		elif !intro_finished:
+			bgm.seek(2.75)
+	if bgm.get_playback_position() > 2.75 and !intro_finished:
+		enable_start()
 
-
+func enable_start():
+	intro_finished = true
+	flash.start(0.5)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -24,3 +34,6 @@ func _on_FlashTimer_timeout():
 		game_start_text.hide()
 	else:
 		game_start_text.show()
+
+
+
