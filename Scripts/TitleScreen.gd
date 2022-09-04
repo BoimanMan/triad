cb800e7588eb95043e3b5df1960a351af72f54af
Alec <84426874+BoimanMan@users.noreply.github.com>
2022-09-04 13:36:00 -0500

    Title screen fix
    Fixed title screen jank

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -7,22 +7,24 @@ extends Control
 onready var flash = $FlashTimer
 onready var bgm = $BGM
 onready var game_start_text = $CanvasLayer/TextureRect/MarginContainer/VBoxContainer/HBoxContainer/Label
-onready var intro_finished = false
+#onready var intro_finished = false
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 	#pass # Replace with function body.
 func _input(event):
 	if event is InputEventKey or event is InputEventMouseButton:
-		if intro_finished:
-			$BGM.stop()
-			get_tree().change_scene("res://Scenes/UI/MainMenu.tscn")
-		elif !intro_finished:
-			bgm.seek(2.75)
-	if bgm.get_playback_position() > 2.75 and !intro_finished:
+		if event.is_pressed():
+			if $BGM.get_playback_position() >= 2.75:
+				$BGM.stop()
+				get_tree().change_scene("res://Scenes/UI/MainMenu.tscn")
+			elif $BGM.get_playback_position() < 2.75:
+				bgm.seek(2.75)
+	
+func _physics_process(delta):
+	if $BGM.get_playback_position() >= 2.75 and flash.is_stopped():
 		enable_start()
-
+		
 func enable_start():
-	intro_finished = true
 	flash.start(0.5)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
